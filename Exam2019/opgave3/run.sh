d06cfaa53b671f05e402c1928307cb06e00f1416
AlbertRossJoh <albertrossj@gmail.com>
2024-12-09 16:37:39 +0100

    opgave3 done

---
+++ Exam2019/opgave3/run.sh
@@ -0,0 +1,10 @@
+
+dotnet fsi -r ./../FsLexYacc.Runtime.dll \
+    Util.fs \
+    Absyn.fs \
+    Machine.fs \
+    Comp.fs \
+    CPar.fs \
+    CLex.fs \
+    Parse.fs 
+
---
+++ Exam2019/opgave3/test.c
@@ -0,0 +1,8 @@
+
+int g;
+void main(){
+    int n;
+    int *p;
+    int *pn[1];
+    int a[2];
+}
