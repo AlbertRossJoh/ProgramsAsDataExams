546603bdf453ba8025e7a52c1eb495a5e171b5cd
AlbertRossJoh <albertrossj@gmail.com>
2024-12-15 22:37:24 +0100

    4.3 done

--- Exam2022Jan/opgave4/Program.fs
+++ Exam2022Jan/opgave4/Program.fs
@@ -1,6 +1,15 @@
 ﻿// For more information see https://aka.ms/fsharp-console-apps
 
 open Icon
+let examEx41 = 
+    Every(
+        Write(
+                FromTo(1, 10)
+        )
+    )
+
+printfn $"%A{examEx41 |> run}"
+printfn "\n"
 let examEx42 = 
     Every(
         Write(
@@ -12,3 +21,20 @@ let examEx42 =
     )
 
 printfn $"%A{examEx42 |> run}"
+printfn "\n"
+let examEx43 = 
+    Every(
+        Write(
+            Prim("*",
+                FromTo(1, 10),
+                And(
+                    Write(CstS "\n"), 
+                    FromTo(1, 10)
+                )
+            )
+        )
+    )
+
+printfn $"%A{examEx43 |> run}"
+printfn "\n"
+
